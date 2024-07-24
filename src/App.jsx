732d3f1e737d9f3e5dcbb75ae4f74af371e4e010
makeda banza <makedabanza16@gmail.com>
2024-07-24 15:28:52 +0200

    All volunteers table

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import SideBar from "./components/SideBar";
+import Home from "./pages/Home";
+import Volunteers from "./pages/Volunteers";
+
+import { useLocation } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const location = useLocation();
+  const pageName = location.pathname.replace(/\//g, "");
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div div className="flex flex-col fixed">
+      <Header />
+      <div className="flex flex-row w-screen">
+        <div className="w-[15%] ">
+          <SideBar />
+        </div>
+        <div className="w-full h-screen overflow-auto bg-[#def5de]">
+          {pageName ? (
+            <h3 className="mt-8 ml-20 text-4xl text-[#1D2130] font-Montserrat font-extrabold">
+              {pageName}{" "}
+            </h3>
+          ) : (
+            <h3 className="mt-8 ml-20 text-4xl text-[#1D2130] font-Montserrat font-extrabold">
+              home
+            </h3>
+          )}
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/volunteers" element={<Volunteers />} />
+          </Routes>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
