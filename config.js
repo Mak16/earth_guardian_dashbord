18c36029662208e8b950b37fcc1500b15979cf0d
makeda banza <makedabanza16@gmail.com>
2024-08-22 21:12:22 +0200

    change API_URL

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 // config.js
-export const API_BASE_URL = 'http://localhost:9000/';
-// export const API_BASE_URL="https://earth-guardian-api-1.onrender.com:10000/"
+// export const API_BASE_URL = 'http://localhost:9000/';
+export const API_BASE_URL="https://earth-guardian-api-1.onrender.com:10000/"
